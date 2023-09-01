1bfd3bcbb5d38d782006d34995261a864a042dd5
Joshua Knauber <joshua.knauber@gmail.com>
2023-09-02 00:13:44 +0200

    remove nid on socket event

--- Tiles App/tiles-app/src/components/hooks/useSidecarEvents.ts
+++ Tiles App/tiles-app/src/components/hooks/useSidecarEvents.ts
@@ -1,56 +1,66 @@
 import { useEffect } from "react";
-import * as v from 'valibot';
+import * as v from "valibot";
 import useWebSocket from "react-use-websocket";
 import useStore from "@/stores/useStore";
 
 const ConnectionSchema = v.object({
-    connected: v.boolean()
-})
+  connected: v.boolean(),
+});
 
 const ReportHidSchema = v.object({
-    nid: v.number([v.integer()]),
-    type: v.number([v.integer()]),
-    hid: v.array(v.number([v.integer()]), [v.length(4)])
-})
+  nid: v.number([v.integer()]),
+  type: v.number([v.integer()]),
+  hid: v.array(v.number([v.integer()]), [v.length(4)]),
+});
 
 const ReportNeighboursSchema = v.object({
-    nid: v.number(),
-    neighbours: v.array(v.number(), [v.length(4)])
-})
+  nid: v.number(),
+  neighbours: v.array(v.number(), [v.length(4)]),
+});
 
 const SendDataSchema = v.object({
-    sender: v.number(),
-    data: v.number()
-})
+  sender: v.number(),
+  data: v.number(),
+});
+
+const DisconnectSchema = v.object({
+  removed_nid: v.number(),
+});
 
 export default function useSidecarEvents() {
-    const { lastJsonMessage } = useWebSocket("ws://localhost:8080");
+  const { lastJsonMessage } = useWebSocket("ws://localhost:8080");
 
-    const setConnected = useStore(state => state.setConnected)
-    const connectTile = useStore(state => state.connectTile)
-    const setNeighbours = useStore(state => state.setNeighbours)
-    const setData = useStore(state => state.setData)
+  const setConnected = useStore((state) => state.setConnected);
+  const connectTile = useStore((state) => state.connectTile);
+  const setNeighbours = useStore((state) => state.setNeighbours);
+  const setData = useStore((state) => state.setData);
+  const disconnectTile = useStore((state) => state.disconnectTile);
 
-    useEffect(() => {
-        // Core Connection Event
-        if (v.safeParse(ConnectionSchema, lastJsonMessage).success) {
-            const data = lastJsonMessage as v.Output<typeof ConnectionSchema>
-            setConnected(data.connected)
-        }
-        // Report Hardware Id Event
-        else if (v.safeParse(ReportHidSchema, lastJsonMessage).success) {
-            let data = lastJsonMessage as v.Output<typeof ReportHidSchema>
-            connectTile(data.nid, data.type, data.hid)
-        }
-        // Report Neighbours Event
-        else if (v.safeParse(ReportNeighboursSchema, lastJsonMessage).success) {
-            let data = lastJsonMessage as v.Output<typeof ReportNeighboursSchema>
-            setNeighbours(data.nid, data.neighbours)
-        }
-        // Send Data Event
-        else if (v.safeParse(SendDataSchema, lastJsonMessage).success) {
-            let data = lastJsonMessage as v.Output<typeof SendDataSchema>
-            setData(data.sender, data.data)
-        }
-    }, [lastJsonMessage])
+  useEffect(() => {
+    // Core Connection Event
+    if (v.safeParse(ConnectionSchema, lastJsonMessage).success) {
+      const data = lastJsonMessage as v.Output<typeof ConnectionSchema>;
+      setConnected(data.connected);
+    }
+    // Report Hardware Id Event
+    else if (v.safeParse(ReportHidSchema, lastJsonMessage).success) {
+      let data = lastJsonMessage as v.Output<typeof ReportHidSchema>;
+      connectTile(data.nid, data.type, data.hid);
+    }
+    // Report Neighbours Event
+    else if (v.safeParse(ReportNeighboursSchema, lastJsonMessage).success) {
+      let data = lastJsonMessage as v.Output<typeof ReportNeighboursSchema>;
+      setNeighbours(data.nid, data.neighbours);
+    }
+    // Send Data Event
+    else if (v.safeParse(SendDataSchema, lastJsonMessage).success) {
+      let data = lastJsonMessage as v.Output<typeof SendDataSchema>;
+      setData(data.sender, data.data);
+    }
+    // Disconnect Event
+    else if (v.safeParse(DisconnectSchema, lastJsonMessage).success) {
+      let data = lastJsonMessage as v.Output<typeof DisconnectSchema>;
+      disconnectTile(data.removed_nid);
+    }
+  }, [lastJsonMessage]);
 }
--- Tiles App/tiles-app/src/stores/useStore.ts
+++ Tiles App/tiles-app/src/stores/useStore.ts
@@ -1,69 +1,86 @@
-import { create } from 'zustand'
+import { create } from "zustand";
 
 interface AppState {
-    connected: boolean;
-    connectedNIDs: number[];
-    nidToHid: Record<number, number[]>;
-    nidToType: Record<number, number>;
-    nidToNeighbours: Record<number, number[]>; // order: top, right, bottom, left
-    nidToData: Record<number, number>;
+  connected: boolean;
+  connectedNIDs: number[];
+  nidToHid: Record<number, number[]>;
+  nidToType: Record<number, number>;
+  nidToNeighbours: Record<number, number[]>; // order: top, right, bottom, left
+  nidToData: Record<number, number>;
 
-    setConnected: (connected: boolean) => void;
-    connectTile: (nid: number, type: number, hid: number[]) => void;
-    setNeighbours: (nid: number, neighbours: number[]) => void;
-    setData: (nid: number, data: number) => void;
+  setConnected: (connected: boolean) => void;
+  connectTile: (nid: number, type: number, hid: number[]) => void;
+  setNeighbours: (nid: number, neighbours: number[]) => void;
+  setData: (nid: number, data: number) => void;
+  disconnectTile: (nid: number) => void;
 }
 
-const useStore = create<AppState>()(
-    (set, get) => ({
-        connected: false,
-        connectedNIDs: [],
-        nidToHid: {},
-        nidToType: {},
-        nidToNeighbours: {},
-        nidToData: {},
+const useStore = create<AppState>()((set, get) => ({
+  connected: false,
+  connectedNIDs: [],
+  nidToHid: {},
+  nidToType: {},
+  nidToNeighbours: {},
+  nidToData: {},
 
-        setConnected: (connected: boolean) => {
-            set({ connected })
-            if (!connected) {
-                clearConnected()
-            }
-        },
-        connectTile: (nid: number, type: number, hid: number[]) => {
-            const newNidToHid = { ...get().nidToHid }
-            newNidToHid[nid] = hid
-            const newNidToType = { ...get().nidToType }
-            newNidToType[nid] = type
-            set({
-                connectedNIDs: Array.from(new Set([...get().connectedNIDs, nid])),
-                nidToHid: newNidToHid,
-                nidToType: newNidToType
-            })
-        },
-        setNeighbours: (nid: number, neighbours: number[]) => {
-            const newNidToNeighbours = { ...get().nidToNeighbours }
-            newNidToNeighbours[nid] = neighbours
-            set({ nidToNeighbours: newNidToNeighbours })
-        },
-        setData: (nid: number, data: number) => {
-            const newNidToData = { ...get().nidToData }
-            newNidToData[nid] = data
-            set({ nidToData: newNidToData })
-        }
-    })
-)
+  setConnected: (connected: boolean) => {
+    set({ connected });
+    if (!connected) {
+      clearConnected();
+    }
+  },
+  connectTile: (nid: number, type: number, hid: number[]) => {
+    const newNidToHid = { ...get().nidToHid };
+    newNidToHid[nid] = hid;
+    const newNidToType = { ...get().nidToType };
+    newNidToType[nid] = type;
+    set({
+      connectedNIDs: Array.from(new Set([...get().connectedNIDs, nid])),
+      nidToHid: newNidToHid,
+      nidToType: newNidToType,
+    });
+  },
+  setNeighbours: (nid: number, neighbours: number[]) => {
+    const newNidToNeighbours = { ...get().nidToNeighbours };
+    newNidToNeighbours[nid] = neighbours;
+    set({ nidToNeighbours: newNidToNeighbours });
+  },
+  setData: (nid: number, data: number) => {
+    const newNidToData = { ...get().nidToData };
+    newNidToData[nid] = data;
+    set({ nidToData: newNidToData });
+  },
+  disconnectTile: (nid: number) => {
+    const newConnectedNIDs = get().connectedNIDs.filter((n) => n !== nid);
+    const newNidToHid = { ...get().nidToHid };
+    delete newNidToHid[nid];
+    const newNidToType = { ...get().nidToType };
+    delete newNidToType[nid];
+    const newNidToNeighbours = { ...get().nidToNeighbours };
+    delete newNidToNeighbours[nid];
+    const newNidToData = { ...get().nidToData };
+    delete newNidToData[nid];
+    set({
+      connectedNIDs: newConnectedNIDs,
+      nidToHid: newNidToHid,
+      nidToType: newNidToType,
+      nidToNeighbours: newNidToNeighbours,
+      nidToData: newNidToData,
+    });
+  },
+}));
 
 /**
  * Resets all state related to the connected tiles
  */
 function clearConnected() {
-    useStore.setState({
-        connectedNIDs: [],
-        connected: false,
-        nidToHid: {},
-        nidToNeighbours: {},
-        nidToType: {}
-    })
+  useStore.setState({
+    connectedNIDs: [],
+    connected: false,
+    nidToHid: {},
+    nidToNeighbours: {},
+    nidToType: {},
+  });
 }
 
-export default useStore
+export default useStore;
