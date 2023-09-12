8d502f03db5a9af3c11edfb2e3457f9d03be5716
Joshua Knauber <joshua.knauber@gmail.com>
2023-09-12 20:39:49 +0200

    send node click event

--- Tiles App/tiles-app/src/components/editor/Editor.tsx
+++ Tiles App/tiles-app/src/components/editor/Editor.tsx
@@ -1,25 +1,49 @@
-import { Background, BackgroundVariant, ReactFlow, SelectionMode } from "reactflow";
+import {
+  Background,
+  BackgroundVariant,
+  Node,
+  ReactFlow,
+  SelectionMode,
+} from "reactflow";
 import useNodes from "./hooks/useNodes";
+import useWebSocket from "react-use-websocket";
+import useStore from "@/stores/useStore";
 
 type EditorProps = {
-    children?: React.ReactNode;
-}
+  children?: React.ReactNode;
+};
 
 export default function Editor({ children }: EditorProps) {
-    const { nodes, nodeTypes } = useNodes()
+  const { nodes, nodeTypes } = useNodes();
+  const nidToHid = useStore((state) => state.nidToHid);
+  const { sendJsonMessage } = useWebSocket("ws://localhost:8080");
 
-    return (
-        <>
-            <ReactFlow
-                selectionMode={SelectionMode.Full}
-                nodeTypes={nodeTypes}
-                nodes={nodes}
-                edges={[]}
-                proOptions={{ hideAttribution: true }}
-            >
-                <Background color="rgba(255, 255, 255, .15)" gap={32} size={2} variant={BackgroundVariant.Dots} />
-                {children}
-            </ReactFlow>
-        </>
-    )
+  const onNodeClick = (event: React.MouseEvent, node: Node) => {
+    const nid = parseInt(node.id);
+    sendJsonMessage({
+      type: "node-select",
+      hid: nidToHid[nid],
+    });
+  };
+
+  return (
+    <>
+      <ReactFlow
+        selectionMode={SelectionMode.Full}
+        nodeTypes={nodeTypes}
+        nodes={nodes}
+        edges={[]}
+        proOptions={{ hideAttribution: true }}
+        onNodeClick={onNodeClick}
+      >
+        <Background
+          color="rgba(255, 255, 255, .15)"
+          gap={32}
+          size={2}
+          variant={BackgroundVariant.Dots}
+        />
+        {children}
+      </ReactFlow>
+    </>
+  );
 }
